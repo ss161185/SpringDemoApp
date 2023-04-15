037c3bf6ec90b6a930adb60f60d21decc4585576
ss161185 <72381039+ss161185@users.noreply.github.com>
2023-04-14 20:34:15 -0400

    Created ProfileRestController.java

---
+++ src/test/java/com/springbootdemo/demoapp/ProfileRestController.java
@@ -0,0 +1,14 @@
+package com.springbootdemo.demoapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProfileRestController {
+
+    @GetMapping("profile")
+    public String Profile(){
+
+        return ("Name: Shubham Sharma "+ "Age: 24 " + "Level Of Study: Master Of Engineering (Software Engineering");
+    }
+}
